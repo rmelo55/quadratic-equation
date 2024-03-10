93ef81b4f4eb1ac4ca28dbc0fa0b237e470d8499
Ricardo Melo <ricardomelo.rt@gmail.com>
2024-03-10 17:50:00 -0300

    update

--- script.js
+++ script.js
@@ -17,11 +17,13 @@ form.addEventListener('submit', (e) => {
         result.innerHTML = 'Existem duas raízes reais e distintas.';
         roots.innerHTML = `x<sub>1</sub> = ${x1}, x<sub>2</sub> = ${x2}`;
         result.classList.add('twoRoots');
+        roots.classList.add('roots-border');
     } else if (delta === 0) {   
         result.innerHTML = 'Existe apenas uma raiz real.';
         const x = -b / (2 * a);
         roots.innerHTML = `x = ${x}`;
-        result.classList.add('oneRoot')
+        result.classList.add('oneRoot');
+        roots.classList.add('roots-border');
     } else {
         result.classList.add('noRoots');
         result.innerHTML = 'Não existem raízes reais.';
