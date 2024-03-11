7ee89fe49ddddca99c06f51963eda07d555b950b
Ricardo Melo <ricardomelo.rt@gmail.com>
2024-03-10 23:58:13 -0300

    update

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const form = document.querySelector('form');
 const result = document.querySelector('.result');
 const roots = document.querySelector('.roots');
 const resultRoots = document.querySelector('.result-roots');
+const vertices = document.querySelector('.vertices');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -15,11 +16,17 @@ form.addEventListener('submit', (e) => {
     if (delta > 0) {
         const x1 = (-b + Math.sqrt(delta)) / (2 * a);
         const x2 = (-b - Math.sqrt(delta)) / (2 * a);
+
+        const vx = -b / (2 * a);
+        const vy = -delta / (4 * a);
+
         result.innerHTML = 'Existem duas raízes reais e distintas.';
         roots.innerHTML = `x<sub>1</sub> = ${x1}, x<sub>2</sub> = ${x2}`;
+        vertices.innerHTML = `O discriminante (&Delta;) é: ${delta} <br>O vértice da parábola está na coordenada: <bolder>(${vx},${vy})</bolder`;
         roots.classList.add('roots-border');
-        result.classList.add('twoRoots');
-        resultRoots.classList.add('result-roots');
+        result.classList.add('result2');
+        resultRoots.classList.add('result-roots2');
+        vertices.classList.add('vertices2');
     } else if (delta === 0) {   
         result.innerHTML = 'Existe apenas uma raiz real.';
         const x = -b / (2 * a);
